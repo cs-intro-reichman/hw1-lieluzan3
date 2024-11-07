cedf746cf6cb63d271b40f4dc89e9e58517e1f5a
lieluzan3 <lieluzan3@gmail.com>
2024-11-07 19:49:23 +0200

    first submission

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,25 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		//input inves money, number of years and rate of return
+		double currentvalue =Integer.parseInt(args[0]);
+	    int years =Integer.parseInt(args[1]);
+		double rate =Double.parseDouble(args[2]);
+		//keep the value of the money in another variable , i am doing the same with the years and the rate.
+		double futurevalue=currentvalue;
+		int yearfirst=years;
+		double Orate=rate;
+	    rate=rate/100+1;
+		//using while loop to getting the invest sum
+		while (years>0) 
+		{ 
+			
+			currentvalue= currentvalue*rate;
+			years--;
+		}
+		//output the future money ater investing with the rate and the initial money value
+
+		System.out.println(" After "+ yearfirst +" years"+" $"+(int)futurevalue+" save at "+ Orate+"%"+" will yield "+"$"+ (int)currentvalue);
+		
 	}
 }
--- TimeFormat.java
+++ TimeFormat.java
@@ -1,4 +1,4 @@
-// Represents the hh:mm time format using an AM/PM format. 
+
 public class TimeFormat {
 	public static void main(String[] args) {
 		// In Java, the command-line arguments args[0], args[1], ... are represented
@@ -10,8 +10,61 @@ public class TimeFormat {
 		// It then concatenates the resulting string with the rightmost hour-digit,
 		// and then uses parseInt to cast the resulting string as an int.
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+		//check situation where hours are bigger than 12
+		if(hours>12){
+		
+			hours=hours-12;
+			//separete situations where the input is a single number in pm
+			if(minutes<10 & hours<10){
+				System.out.println("0"+hours+":"+"0"+minutes+" PM");
+			}
+			else if(minutes<10){
+				System.out.println(+hours+":"+"0"+minutes+" PM");
+			}
+			else if(hours<10){
+				System.out.println("0"+hours+":"+minutes+" PM");
+			}
+
+			else
+
+				System.out.println(hours+":"+minutes+" PM");
+		
+		}
+		//check situation when the hours are 12 exactly
+		else if(hours==12){
+			if(minutes<10)
+
+				System.out.println(hours+":"+"0"+minutes+" PM");
+			else
+
+				System.out.println(hours+":"+"0"+minutes+" PM");
+		}
+		//check situation when the hours and the minuts under 10 both
+		else if(minutes<10 & hours<10){
+
+			System.out.println("0"+hours+ ":"+"0"+minutes+" AM");
+
+		}
+		//check just if the hours under 10
+		else if(hours<10){
+
+			System.out.println("0"+hours+ ":"+minutes+" AM");
+
+		}
+		//check just if the minutes under 10
+		else if(minutes<10){
+			System.out.println(hours+ ":"+"0"+minutes+" AM");
+
+		}
+		
+			
+		 
+		else
+			
         // Replace this comment with the rest of your code
+		System.out.println(hours+ ":"+minutes+" AM");
 	}
 }
